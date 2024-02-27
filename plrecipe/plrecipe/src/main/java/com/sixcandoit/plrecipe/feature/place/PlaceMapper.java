44638e02cdf161b1e0b8b6de00bdf3a02304f161
hso <sh07202@naver.com>
2024-02-27 18:19:30 +0900

    [UPDATE] place - add findStarByPlace/findPlaceByCategory

--- plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/place/PlaceMapper.java
+++ plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/place/PlaceMapper.java
@@ -7,4 +7,8 @@ import java.util.List;
 @Mapper
 public interface PlaceMapper {
     List<PlaceDTO> selectAllPlace();
+
+    List<PlaceDTO> selectPlaceByCategory(String categoryName);
+
+    List<PlaceStarDTO> selectStarByPlace(int placeId);
 }
--- plrecipe/plrecipe/src/test/java/com/sixcandoit/plrecipe/feature/place/PlaceServiceTests.java
+++ plrecipe/plrecipe/src/test/java/com/sixcandoit/plrecipe/feature/place/PlaceServiceTests.java
@@ -1,12 +1,15 @@
 package com.sixcandoit.plrecipe.feature.place;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class PlaceServiceTests {
@@ -15,12 +18,30 @@ public class PlaceServiceTests {
     private Place placeService;
 
     @Test
-    void testRegistNewOrder() {
+    void findAllPlace() {
+        List<PlaceDTO> placeList = placeService.selectAllPlace();
+        placeList.forEach(System.out::println);
 
-        List<PlaceDTO> place = placeService.selectAllPlace();
-        place.forEach(System.out::println);
-
-        assertNotNull(place);
+        assertTrue(!placeList.isEmpty());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"음식점","카페", "문화", "액티비티", "산책", "기타"})
+    void findPlaceByCategory(String categoryName){
+
+        List<PlaceDTO> placeList = placeService.selectPlaceByCategory(categoryName);
+        placeList.forEach(System.out::println);
+
+        assertTrue(!placeList.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void findStarByPlace(int placeId){
+
+        List<PlaceStarDTO> placeList = placeService.selectStarByPlace(placeId);
+        placeList.forEach(System.out::println);
+
+        assertNotNull(!placeList.isEmpty());
+    }
 }
