0d3d60b25a71e6527a572575be1f82c8b514c47b
조수빈 <angelajsb@naver.com>
2024-02-27 18:04:53 +0900

    member 전체 조회

--- plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/member/MemberMapper.java
+++ plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/member/MemberMapper.java
@@ -1,4 +1,10 @@
 package com.sixcandoit.plrecipe.feature.member;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface MemberMapper {
+    List<MemberDTO> selectAllMember();
 }
