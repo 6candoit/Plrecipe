88f87b4b1c165324e0df12b5aac61b7317e9c5de
조수빈 <angelajsb@naver.com>
2024-02-27 21:36:04 +0900

    feature/follow Follow Update

--- plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/follow/Follow.java
+++ plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/follow/Follow.java
@@ -1,4 +1,15 @@
 package com.sixcandoit.plrecipe.feature.follow;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//@Service
 public class Follow {
+//    private final FollowMapper followMapper;
+//
+//    @Autowired
+//    public Follow(FollowMapper followMapper) {
+//        this.followMapper = followMapper;}
+
+
 }
