f26638544fb6dfdf925058da70990a0368bf8d80
조수빈 <angelajsb@naver.com>
2024-02-27 21:35:27 +0900

    feature/member Follow Follower Like Update

--- plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/member/MemberMapper.java
+++ plrecipe/plrecipe/src/main/java/com/sixcandoit/plrecipe/feature/member/MemberMapper.java
@@ -6,4 +6,10 @@ import java.util.List;
 @Mapper
 public interface MemberMapper {
     List<MemberDTO> selectAllMember();
+
+    List<MemberDTO> selectMemberByFollower(String memberEmail);
+
+    List<MemberDTO> selectMemberByFollowing(String memberEmail);
+
+    List<MemberDTO> selectMemberByLikePost(String memberEmail);
 }
