f1c98aa90af6711690e51706519f8b0afd96be55
hso <sh07202@naver.com>
2024-02-29 17:36:52 +0900

    [CREATE] plrecipe place

---
+++ plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/PlrecipePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlrecipePlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlrecipePlaceApplication.class, args);
+	}
+
+}
---
+++ plrecipe_place/src/test/java/com/sixcandoit/plrecipe_place/PlrecipePlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlrecipePlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
