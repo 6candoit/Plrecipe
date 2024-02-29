b83ae3269872349741164a28fadb0b3017cbbb75
cookeeholic <cookeeholic@naver.com>
2024-02-29 17:30:00 +0900

    plrecipe_group file upload

---
+++ plrecipe_group/src/main/java/com/sixcandoit/plrecipe_group/PlrecipeGroupApplication.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlrecipeGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlrecipeGroupApplication.class, args);
+    }
+
+}
---
+++ plrecipe_group/src/test/java/com/sixcandoit/plrecipe_group/PlrecipeGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlrecipeGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
