e78deb204ef092b7b23fb1d8828340645e3d88a4
조수빈 <angelajsb@naver.com>
2024-02-29 17:19:35 +0900

    plrecipe_member file upload

---
+++ plrecipe_member/src/main/java/com/sixcandoit/plrecipe_member/PlrecipeMemberApplication.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlrecipeMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlrecipeMemberApplication.class, args);
+    }
+
+}
---
+++ plrecipe_member/src/test/java/com/sixcandoit/plrecipe_member/PlrecipeMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixcandoit.plrecipe_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlrecipeMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
