032bb1fa08338be6536995d255081b104a171d5d
mini-xi <gam1205@naver.com>
2024-03-04 19:00:35 +0900

    group create 기능 추가

--- plrecipe_group/src/main/java/com/sixcandoit/plrecipe_group/service/GroupService.java
+++ plrecipe_group/src/main/java/com/sixcandoit/plrecipe_group/service/GroupService.java
@@ -3,6 +3,8 @@ package com.sixcandoit.plrecipe_group.service;
 import com.sixcandoit.plrecipe_group.dto.GroupDTO;
 import com.sixcandoit.plrecipe_group.dto.MemberDTO;
 import com.sixcandoit.plrecipe_group.repository.mapper.GroupMapper;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,9 @@ import java.util.List;
 public class GroupService {
 
     private GroupMapper groupMapper;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     public GroupService(GroupMapper groupMapper) {
@@ -31,5 +36,15 @@ public class GroupService {
     public List<GroupDTO> selectPublicOrPrivateGroup(String memberEmail){
         return groupMapper.selectPublicOrPrivateGroup(memberEmail);
 
+    }
+
+    @Transactional
+    public GroupDTO createGroup(String groupName) {
+        GroupDTO group = new GroupDTO();
+        group.setGroupName(groupName);
+
+        entityManager.persist(group);
+
+        return group;
     }
 }
