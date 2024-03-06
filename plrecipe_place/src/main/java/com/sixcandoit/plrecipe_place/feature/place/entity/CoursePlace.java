dc31238438da6ec8951e2ed85de3b744d78e2f5e
hso <sh07202@naver.com>
2024-03-06 16:26:43 +0900

    [UPDATE] registCoursePlace test update

--- plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/entity/CoursePlace.java
+++ plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/entity/CoursePlace.java
@@ -1,11 +1,13 @@
 package com.sixcandoit.plrecipe_place.feature.place.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
 @Entity(name = "plrecipe_course_and_place")
 @Table(name = "course_place")
+@Data
 public class CoursePlace {
 
     @Id
@@ -29,38 +31,6 @@ public class CoursePlace {
         this.placeCourseId = placeCourseId;
         this.courseId = courseId;
         this.placeId = placeId;
-        this.sequence = sequence;
-    }
-
-    public int getPlaceCourseId() {
-        return placeCourseId;
-    }
-
-    public void setPlaceCourseId(int placeCourseId) {
-        this.placeCourseId = placeCourseId;
-    }
-
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
-
-    public int getPlaceId() {
-        return placeId;
-    }
-
-    public void setPlaceId(int placeId) {
-        this.placeId = placeId;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(int sequence) {
         this.sequence = sequence;
     }
 
--- plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/service/CourseServiceImpl.java
+++ plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/service/CourseServiceImpl.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,28 +49,60 @@ public class CourseServiceImpl implements CourseService {
         return courseMapper.getPlacesByCourseName(courseId);
     }
 
-
     @Override
-    public void registCoursePlace(CourseDTO course, List<Place> placeList) {
-
-        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    public void registCourse(CourseAndPlace cp) {
 
         /* 코스 정보 저장 */
-        Course newCourse = mapper.map(course, Course.class);
+        Course newCourse = cp.getCourse();
         courseRepository.save(newCourse);
+        System.out.println("newCourse = " + cp);
+
+        /* 코스장소 정보 insert */
+        saveCoursePlace(newCourse.getCourseId(), cp.getPlaces());
+    }
+
+    public void saveCoursePlace(int courseId, List<Place> placeList){
 
         /* 코스id와 장소id를 중간객체 리스트로 만들어서 저장 */
         List<CoursePlaceDTO> coursePlaceList = new ArrayList<>();
         for (int i = 0; i < placeList.size(); i++) {
-            CoursePlaceDTO coursePlaceDTO = new CoursePlaceDTO(newCourse.getCourseId(), placeList.get(i).getPlaceId(), i+1);
+            CoursePlaceDTO coursePlaceDTO = new CoursePlaceDTO(courseId, placeList.get(i).getPlaceId(), i+1);
             coursePlaceList.add(coursePlaceDTO);
         }
 
         /* 코스장소 정보 저장 */
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         List<CoursePlace> coursePlaces = coursePlaceList.stream()
                 .map(coursePlace -> mapper.map(coursePlace, CoursePlace.class))
                 .collect(Collectors.toList());
         coursePlaceRepository.saveAll(coursePlaces);
+
+    }
+
+    @Transactional
+    public void modifyCourse(CourseAndPlace modifyCP) {
+
+        int courseId = modifyCP.getCourse().getCourseId();
+        Course foundCourse = courseRepository.findById(courseId).orElseThrow(IllegalArgumentException::new);
+        foundCourse.setCourseName(modifyCP.getCourse().getCourseName());
+
+        deleteCoursePlace(courseId);
+        saveCoursePlace(courseId, modifyCP.getPlaces());
+
+    }
+
+    @Transactional
+    public void deleteCoursePlace(int courseId){
+        coursePlaceRepository.deleteAllByCourseId(courseId);
+    }
+
+    @Transactional
+    public void deleteCourse(CourseAndPlace deleteCP) {
+
+        int courseId = deleteCP.getCourse().getCourseId();
+        deleteCoursePlace(courseId);
+        courseRepository.deleteById(courseId);
+
     }
 
     /* 멤버id로 멤버가 작성한 코스 리스트 select */
