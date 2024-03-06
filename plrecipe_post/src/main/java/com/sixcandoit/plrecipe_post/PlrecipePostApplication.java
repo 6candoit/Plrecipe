e29f4aded5b88e807feb87225ded0628d235fbda
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-03-06 22:57:05 +0900

    24.03.06 Plrecipe
    서버, 게이트웨이, 서비스 기본 세팅

--- plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/PlrecipePostApplication.java
+++ plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/PlrecipePostApplication.java
@@ -3,9 +3,11 @@ package com.sixcandoit.plrecipe_post;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlrecipePostApplication {
 
 	public static void main(String[] args) {
