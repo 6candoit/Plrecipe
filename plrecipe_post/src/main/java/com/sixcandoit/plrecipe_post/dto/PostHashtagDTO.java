26d70cc3d6ff1368e4837e36467f8976272c40f9
yunjaeeun <yunjaeeun12@naver.com>
2024-03-08 10:22:32 +0900

    24.03.08 plrecipe_post
    포스트 해시태크 수정

--- plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/dto/PostHashtagDTO.java
+++ plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/dto/PostHashtagDTO.java
@@ -2,51 +2,12 @@ package com.sixcandoit.plrecipe_post.dto;
 
 import com.sixcandoit.plrecipe_post.aggregate.Post;
 import com.sixcandoit.plrecipe_post.vo.hashtag.Hashtag;
+import lombok.Data;
 
+@Data
 public class PostHashtagDTO {
     private int postHashtagId;
-    private Post post;
-    private Hashtag hashtag;
+    private PostDTO postDTO;
+    private HashtagDTO hashtagDTO;
 
-    public PostHashtagDTO() {
-    }
-
-    public PostHashtagDTO(int postHashtagId, Post post, Hashtag hashtag) {
-        this.postHashtagId = postHashtagId;
-        this.post = post;
-        this.hashtag = hashtag;
-    }
-
-    public int getPostHashtagId() {
-        return postHashtagId;
-    }
-
-    public void setPostHashtagId(int postHashtagId) {
-        this.postHashtagId = postHashtagId;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    public Hashtag getHashtag() {
-        return hashtag;
-    }
-
-    public void setHashtag(Hashtag hashtag) {
-        this.hashtag = hashtag;
-    }
-
-    @Override
-    public String toString() {
-        return "PostHashtagDTO{" +
-                "postHashtagId=" + postHashtagId +
-                ", post=" + post +
-                ", hashtag=" + hashtag +
-                '}';
-    }
 }
