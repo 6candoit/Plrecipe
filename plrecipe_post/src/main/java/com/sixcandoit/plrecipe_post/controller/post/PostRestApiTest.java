bdfcb21c8c79589c41d78eed180aa14a69002c60
DESKTOP-5F83FJI\User <yunjaeeun12@naver.com>
2024-03-10 19:24:29 +0900

    24.03.10 Plrecipe
    주석 추가

--- plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/controller/post/PostRestApiTest.java
+++ plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/controller/post/PostRestApiTest.java
@@ -41,37 +41,43 @@ public class PostRestApiTest {
         this.postHashtagService = postHashtagService;
     }
 
-
+    /* 설명. 게시글 전체 조회 */
     @GetMapping("/posts")
     public List<PostDTO> selectAllPost() {
         return postService.selectAllPost();
     }
 
+    /* 설명. 멤버가 작성한 게시글 조회(Feign Client를 사용하여 멤버 정보까지 불러옴) */
     @GetMapping("/posts/{memberId}")
     List<PostDTO> selectMemberPosts(@PathVariable int memberId) {
         return postService.selectPostByMember(memberId);
     }
 
+    /* 설명. 게시글 공개 상태별 조회 */
     @GetMapping("/status/{postStatus}")
     public List<PostDTO> selectPostsByStatus(@PathVariable("postStatus") String postStatus){
         return postService.selectPostsByStatus(postStatus);
     }
 
+    /* 게시글 인원수별 조회 */
     @GetMapping("/memberCounts/{memberCounts}")
     public List<PostDTO> selectPostsByMemberCounts(@PathVariable("memberCounts") MemberCount memberCounts) {
         return postService.selectPostsByMemberCounts(memberCounts);
     }
 
+    /* 게시글에 작성된 해시태그 조회 */
     @GetMapping("/hashtag/{postId}")
     public List<PostHashtagDTO> selectPostHashtags(@PathVariable int postId) {
         return postService.selectPostHashtags(postId);
     }
 
+    /* 게시글별 좋아요 조회 */
     @GetMapping("/likes/{postId}")
     public List<PostLikeDTO> selectPostsByLikes(@PathVariable int postId) {
         return postService.selectPostByLikes(postId);
     }
 
+    /* 해시태그 생성 */
     @PostMapping("/regist/hashtag")
     private ResponseEntity<ResponseHashtag> registHashTag(@RequestBody RequestHashtag hashtag) {
         HashtagDTO hashtagDTO = modelMapper.map(hashtag, HashtagDTO.class);
@@ -83,17 +89,7 @@ public class PostRestApiTest {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseHashtag);
     }
 
-    @PostMapping("/regist")
-    private ResponseEntity<ResponsePost> registPost(@RequestBody RequestPost post) {
-        PostDTO postDTO = modelMapper.map(post, PostDTO.class);
-
-        postService.registPost(postDTO);
-
-        ResponsePost responsePost = modelMapper.map(postDTO, ResponsePost.class);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(responsePost);
-    }
-
+    /* 게시글 생성(해시태그 중간 객체도 같이 insert) */
     @PostMapping("/regist_both")
     private ResponseEntity<?> registPostAndHashtag(@RequestBody RequestTest test) {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
@@ -103,20 +99,23 @@ public class PostRestApiTest {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /* 게시글 수정 */
     @PatchMapping("/modify/{postId}")
     public ResponseEntity<Post> modifyPost (@RequestBody RequestPost requestPost, @PathVariable int postId ) {
         return ResponseEntity.ok(postService.modifyPost(postId, requestPost));
     }
 
+    /* 게시글 삭제(게시글 삭제일 update) */
     @PatchMapping("/delete/{postId}")
     public ResponseEntity<Post> deletePost (@RequestBody RequestPost requestPost, @PathVariable int postId) {
         return ResponseEntity.ok(postService.modifyPostDeleteDate(postId, requestPost));
     }
 
-
+    /* 해시태그 삭제 */
     @GetMapping("/hashtag/delete")
     public void deletePage() {}
 
+    /* 해시태그 중간객체 삭제 */
     @DeleteMapping("/hashtag/delete/{postHashtagId}")
     public ResponseEntity<PostHashtag> deletePostHashTag(@PathVariable int postHashtagId) {
         postHashtagService.deletePost(postHashtagId);
@@ -124,11 +123,24 @@ public class PostRestApiTest {
         return ResponseEntity.ok().build();
     }
 
+    /* 게시글 좋아요 등록 */
     @PostMapping("/like")
     private ResponseEntity<ResponsePost> postLike(@RequestBody RequestPost post) {
         PostDTO postDTO = modelMapper.map(post, PostDTO.class);
 
         postService.postLike(postDTO);
+
+        ResponsePost responsePost = modelMapper.map(postDTO, ResponsePost.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responsePost);
+    }
+
+    /*  */
+    @PostMapping("/regist")
+    private ResponseEntity<ResponsePost> registPost(@RequestBody RequestPost post) {
+        PostDTO postDTO = modelMapper.map(post, PostDTO.class);
+
+        postService.registPost(postDTO);
 
         ResponsePost responsePost = modelMapper.map(postDTO, ResponsePost.class);
 
--- plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/service/PostServiceImpl.java
+++ plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/service/PostServiceImpl.java
@@ -166,7 +166,6 @@ public class PostServiceImpl implements PostService {
         return postMapper.selectAllPost();
     }
 
-    /* 제발 푸쉬좀123131 */
     @Override
     public List<PostDTO> selectPostByMember(int memberId) {
         List<PostDTO> postList = postMapper.selectMemberPosts(memberId);
