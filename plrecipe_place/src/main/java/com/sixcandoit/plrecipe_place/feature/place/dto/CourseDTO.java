f64e3e3d505849f6b897018d66bc14e1484efd27
hso <sh07202@naver.com>
2024-03-11 10:33:56 +0900

    [MODIFY] @Data edit

--- plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/dto/CourseDTO.java
+++ plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/dto/CourseDTO.java
@@ -3,7 +3,7 @@ package com.sixcandoit.plrecipe_place.feature.place.dto;
 import com.sixcandoit.plrecipe_place.feature.place.vo.ResponseMember;
 import lombok.Data;
 
-@Data
+
 public class CourseDTO {
     private int courseId;
     private String courseName;
@@ -13,5 +13,47 @@ public class CourseDTO {
     public CourseDTO() {
     }
 
+    public CourseDTO(int courseId, String courseName, ResponseMember member) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.member = member;
+    }
 
+    public CourseDTO(String courseName, ResponseMember member) {
+        this.courseName = courseName;
+        this.member = member;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public ResponseMember getMember() {
+        return member;
+    }
+
+    public void setMember(ResponseMember member) {
+        this.member = member;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseDTO{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", member=" + member +
+                '}';
+    }
 }
--- plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/entity/CoursePlace.java
+++ plrecipe_place/src/main/java/com/sixcandoit/plrecipe_place/feature/place/entity/CoursePlace.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 @Entity(name = "plrecipe_course_and_place")
 @Table(name = "course_place")
-@Data
 public class CoursePlace {
 
     @Id
@@ -31,6 +30,44 @@ public class CoursePlace {
         this.placeCourseId = placeCourseId;
         this.courseId = courseId;
         this.placeId = placeId;
+        this.sequence = sequence;
+    }
+
+    public CoursePlace(int courseId, int placeId, int sequence) {
+        this.courseId = courseId;
+        this.placeId = placeId;
+        this.sequence = sequence;
+    }
+
+    public int getPlaceCourseId() {
+        return placeCourseId;
+    }
+
+    public void setPlaceCourseId(int placeCourseId) {
+        this.placeCourseId = placeCourseId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(int placeId) {
+        this.placeId = placeId;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
         this.sequence = sequence;
     }
 
