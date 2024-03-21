3ce8be859bfdf8fb1256bae6f91afecdd1825597
yunjaeeun <yunjaeeun12@naver.com>
2024-03-21 18:13:09 +0900

    24.03.21 plrecipe_post
    cors 적용

---
+++ plrecipe_post/src/main/java/com/sixcandoit/plrecipe_post/command/configuration/test.java
@@ -0,0 +1,15 @@
+package com.sixcandoit.plrecipe_post.command.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class test implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://127.0.0.1:5500");
+    }
+}
